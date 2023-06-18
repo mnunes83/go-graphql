d1bfdd1d6dfb949b899911c405f6c5b21c818a4f
Mauricio <nunes.mauricio@gmail.com>
2023-06-18 01:32:25 -0300

    Implementação GraphQL

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -16,13 +16,14 @@ import (
 const defaultPort = "8080"
 
 func main() {
-	db, err := sql.Open("sqlite3", "/.data.db")
+	db, err := sql.Open("sqlite3", "data.db")
 	if err != nil {
 		log.Fatalf("failed to open datanase: %v", err)
 	}
 	defer db.Close()
 
 	categoryDB := database.NewCategory(db)
+	courseDB := database.NewCourse(db)
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -31,6 +32,7 @@ func main() {
 
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
 		CategoryDB: categoryDB,
+		CourseDB:   courseDB,
 	}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
--- graph/resolver.go
+++ graph/resolver.go
@@ -8,4 +8,5 @@ import "github/mnunes83/13-GraphQL/internal/database"
 
 type Resolver struct {
 	CategoryDB *database.Category
+	CourseDB   *database.Course
 }
